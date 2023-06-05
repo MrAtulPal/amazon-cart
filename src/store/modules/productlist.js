2c4b21de2563b58ff4f427da757d2d49a73c921e
Atul Pal <mr.atulofficial.in@gmail.com>
2023-06-05 13:05:58 +0530

    modification done

--- src/store/modules/productlist.js
+++ src/store/modules/productlist.js
@@ -1,50 +1,54 @@
-const state ={
-    productsarray:[],
-    cart:[],
-    saveforlater:[]
-}
+const state = {
+  productsarray: [],
+  cart: [],
+  saveforlater: [],
+};
 
-const getters={
-    allproducts:(state)=> state.productsarray,
-    getCartItem:(state)=>state.cart,
-    getlater:(state)=>state.saveforlater
-}
+const getters = {
+  allproducts: (state) => state.productsarray,
+  getCartItem: (state) => state.cart,
+  getlater: (state) => state.saveforlater,
+};
 
-const actions={
-    async getProducts({commit}){
-        const res = await fetch('http://localhost:3000/products')
-        const productdata = await res.json()
-        commit('setProducts',productdata)
-    },
-    updateCartItem({commit},item){
-         commit('setCartItem',item)
-    },
-    removeCartItem({ commit }, itemId) {
-      commit('removeItem', itemId);
-    },
-    updateLater({commit},item){
-        commit('setLaterItem',item)
-    },
-    removeLater({commit},item){
-        commit('setRemoveLater',item)
-    }
-}
+const actions = {
+  async getProducts({ commit }) {
+    const res = await fetch("http://localhost:3000/products");
+    const productdata = await res.json();
+    commit("setProducts", productdata);
+  },
 
-const mutations={
-    setProducts:(state,product)=>(state.productsarray=product),
-    setCartItem:(state,item)=>(state.cart.push(item)),
-    removeItem(state, itemId) {
-        state.cart = state.cart.filter(item => item.id !== itemId);
-      },
-    setLaterItem:(state,item)=>(state.saveforlater.push(item))
-    , setRemoveLater(state, itemId) {
-        state.saveforlater = state.saveforlater.filter(item => item !== itemId);
-      }
-}
+  //cart operation
+  updateCartItem({ commit }, item) {
+    commit("setCartItem", item);
+  },
+  removeCartItem({ commit }, itemId) {
+    commit("removeItem", itemId);
+  },
 
-export default{
-    state,
-    getters,
-    actions,
-    mutations
-}
+// save for later operation
+  updateLater({ commit }, item) {
+    commit("setLaterItem", item);
+  },
+  removeLater({ commit }, item) {
+    commit("setRemoveLater", item);
+  },
+};
+
+const mutations = {
+  setProducts: (state, product) => (state.productsarray = product),
+  setCartItem: (state, item) => state.cart.push(item),
+  removeItem(state, itemId) {
+    state.cart = state.cart.filter((item) => item.id !== itemId);
+  },
+  setLaterItem: (state, item) => state.saveforlater.push(item),
+  setRemoveLater(state, itemId) {
+    state.saveforlater = state.saveforlater.filter((item) => item !== itemId);
+  },
+};
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations,
+};
